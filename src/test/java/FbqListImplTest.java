a53ffc14779ef5643906485e3051bd879d6396d3
EricONDOA <eric.ondoa@gmail.com>
2016-05-26 07:09:02 +0200

    Add easyTesting, and add more TU

--- src/test/java/FbqListImplTest.java
+++ src/test/java/FbqListImplTest.java
@@ -1,8 +1,17 @@
 
 
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import services.IFbqList;
+import type.Fbq;
+
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.*;
 
 
 /**
@@ -13,6 +22,38 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/spring-config.xml"})
 public class FbqListImplTest {
+
+
+    @Autowired
+    IFbqList iFbqList;
+
+    @Test
+    public void test_calculateAll_Should_Return0Result_when_enter_wrong_number(){
+       // Given
+        Integer number = 0;
+
+       //When
+        List<Fbq> results = iFbqList.calculateALL(number);
+
+       //Then
+        assertThat(results).isEmpty();
+
+    }
+
+    @Test
+    public void test_calculteAll_Should_Test_The_fonctional_method() {
+        // Given
+        Integer maxNumber = 100;
+
+        // When
+        List<Fbq> results = iFbqList.calculateALL(maxNumber);
+
+        // Then
+        assertThat(results).isNotNull();
+        assertThat(results).isNotEmpty();
+        assertThat(results.get(2).getNumber()).isEqualTo(3);
+        assertThat(results.get(2).getValue()).isEqualTo("FooFoo");
+    }
 
 
 }
